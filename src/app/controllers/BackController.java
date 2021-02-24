68220341ca52f483678a23476f67b7becf15cd4d
JeanCarlosSC <jeancarlosodi@gmail.com>
2021-02-24 09:55:24 -0500

    lib.sRAD actualizada, se implementa método addMovimiento, se dejan más tareas en BackController y se corrigen algunos bugs

--- src/app/controllers/BackController.java
+++ src/app/controllers/BackController.java
@@ -34,7 +34,28 @@ public class BackController {
     }
 
     public static void insertarProducto(String nombre, Double precio, int stock, int stockMinimo) {
-        //por implementar :D
+        //por implementar
+    }
+
+    public static String[] getNombresProductos() {
+        //por implementar
+        String[] productos = {"Arroz", "Papa", "Néctar"}; //valores para hacer pruebas (mientras se implementa)
+        return productos;
+    }
+
+    public static double getPrecio(String nombreProducto) {
+        //por implementar
+        System.out.println(nombreProducto);//para hacer pruebas
+        return 1000.0;
+    }
+
+    public static int getStock(String nombreProducto) {
+        //por implementar
+        return 0;
+    }
+
+    public static void insertarMovimiento(String nombreProducto, int cantidad) {
+        //por implementar
     }
 }
 
--- src/app/gui/View.java
+++ src/app/gui/View.java
@@ -6,8 +6,10 @@ import lib.sRAD.gui.sComponent.*;
 
 import javax.swing.*;
 
-import static lib.sRAD.logic.Extension.isDouble;
-import static lib.sRAD.logic.Extension.isInt;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static lib.sRAD.logic.Extension.*;
 
 public class View {
     public static View view;
@@ -88,6 +90,7 @@ public class View {
             else {
                 JOptionPane.showMessageDialog(null, "Por favor ingrese valores válidos", "Error", JOptionPane.ERROR_MESSAGE);
             }
+            ventana.cerrar();
         });
         ventana.add(btConfirm);
 
@@ -99,7 +102,68 @@ public class View {
     }
 
     public void addMovimiento() {
+        VentanaEmergente ventana = new VentanaEmergente(Controller.controller, 340, 300);
 
+        SLabel lInsertar = new SLabel(32, 32, 200, 28, "Inserte un movimiento");
+        ventana.add(lInsertar);
+
+        SLabel lTipo = new SLabel(64, 64, 168, 28, "Producto:");
+        ventana.add(lTipo);
+
+        SLabel lPrecioText = new SLabel(64, 104, 168, 28, "Precio:");
+        ventana.add(lPrecioText);
+
+        AtomicReference<Double> precio = new AtomicReference<>(0.0);
+        AtomicReference<Double> precioTotal = new AtomicReference<>(0.0);
+        AtomicInteger cantidad = new AtomicInteger();
+
+        SLabel lPrecio = new SLabel(200, 102, 100, 32, "");
+        ventana.add(lPrecio);
+
+        SLabel lCantidad = new SLabel(64, 144, 168, 28, "Cantidad:");
+        ventana.add(lCantidad);
+
+        SLabel lPrecioTotal = new SLabel(200, 182, 100, 32, "");
+        ventana.add(lPrecioTotal);
+
+        STextField tfCantidad = new STextField(200, 142, 100, 32);
+        tfCantidad.addActionListener( (e) -> {
+            if (isInt(tfCantidad.getText()) && Integer.parseInt(tfCantidad.getText()) > 0) {
+                cantidad.set(Integer.parseInt(tfCantidad.getText()));
+                precioTotal.set(cantidad.get() * precio.get());
+                lPrecioTotal.setText(toCOP(precioTotal.get()));
+            }
+        });
+        ventana.add(tfCantidad);
+
+        SLabel lPrecioTotalText = new SLabel(64, 184, 168, 28, "Precio Total:");
+        ventana.add(lPrecioTotalText);
+
+        String[] opciones = BackController.getNombresProductos();
+        SComboBox cbTipo = new SComboBox(SComboBox.DECORADO, 200, 62, 100, 32, opciones);
+        cbTipo.addActionListener( (e) -> {
+            precio.set(BackController.getPrecio(cbTipo.getItemAt(cbTipo.getSelectedIndex()).toString()));
+            lPrecio.setText(toCOP(precio.get()));
+        });
+        ventana.add(cbTipo);
+
+        SButton btConfirm = new SButton(50, 232, 100, 32, "INSERTAR");
+        btConfirm.addActionListener( (e) -> {
+            if (cantidad.get()>0 && cantidad.get()>BackController.getStock(cbTipo.getItemAt(cbTipo.getSelectedIndex()).toString())) {
+                BackController.insertarMovimiento(cbTipo.getItemAt(cbTipo.getSelectedIndex()).toString(), cantidad.get());
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Por favor ingrese valores válidos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            ventana.cerrar();
+        });
+        ventana.add(btConfirm);
+
+        SButton btClose = new SButton(190, 232, 100, 32, "CERRAR");
+        btClose.addActionListener( (e) -> ventana.cerrar());
+        ventana.add(btClose);
+
+        ventana.lanzar();
     }
 
     public static void init() {
