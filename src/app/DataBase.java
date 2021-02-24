bce94b77cfbdbb8f14cd47625d1bccb259a9f39c
Jesús Bermúdez <jema0622@outlook.com>
2021-02-23 22:56:01 -0500

    Se agregan clases para la manipulacion de base de datos

--- src/app/DataBase.java
+++ src/app/DataBase.java
@@ -3,47 +3,109 @@ package app;
 import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataBase {
 
-    private static Connection connection;
-    private static DataBase dataBase;
+    private Connection connection;
 
-    private DataBase(String user, String password) throws SQLException, ClassNotFoundException {
+    public DataBase(String user, String password) throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
 
         Properties properties = new Properties();
 
         properties.setProperty("user", user);
         properties.setProperty("password", password);
-        
-        
-        connection = DriverManager.getConnection("jdbc:postgresql://localhost/Sales System", properties);
+
+        this.connection = DriverManager.getConnection("jdbc:postgresql://localhost/Sales System", properties);
     }
 
-    public static void query(String query) throws SQLException {
+    public ResultSet query(String query) throws SQLException {
 
-        Statement statement = connection.createStatement();
+        Statement statement = this.connection.createStatement();
 
         ResultSet response = statement.executeQuery(query);
 
-        while(response.next()) {
-            System.out.println(response.getString("nombre"));
+        statement.close();
+
+        return response;
+    }
+
+    public ResultSet query(String query, Object[] objects) throws SQLException {
+
+        String className;
+
+        PreparedStatement statement = this.connection.prepareStatement(query);
+
+        for(int i = 0; i < objects.length; ++i) {
+            className = objects[i].getClass().getSimpleName();
+
+            switch(className) {
+
+                case "Byte" -> statement.setByte(i + 1, (Byte) objects[i]);
+
+                case "Short" -> statement.setShort(i + 1, (Short) objects[i]);
+
+                case "Integer" -> statement.setInt(i + 1, (Integer) objects[i]);
+
+                case "Long" -> statement.setLong(i + 1, (Long) objects[i]);
+
+                case "Float" -> statement.setFloat(i + 1, (Float) objects[i]);
+
+                case "Double" -> statement.setDouble(i + 1, (Double) objects[i]);
+
+                case "String" -> statement.setString(i + 1, (String) objects[i]);
+
+                case "Boolean" -> statement.setBoolean(i + 1, (Boolean) objects[i]);
+            
+                default -> statement.setNull(i + 1, 0);
+    
+            }
+
         }
 
-        response.close();
+
+        ResultSet response = statement.executeQuery();
+
         statement.close();
+
+        return response;
     }
 
-    public static int validarIngreso(String username, String password) {
-        return 1;
+    public ResultSet getAll(String table) throws SQLException {
+
+        String query = "SELECT * FROM " + table + ';';
+
+        return this.query(query);
     }
 
-    public static void connect(String user, String password) throws ClassNotFoundException, SQLException {
-        dataBase = new DataBase(user, password);
+    public ResultSet getByID(String table, String id) throws SQLException {
+
+        String query = "SELECT * FROM " + table + " WHERE " + this.getIDTable(table) + ';';
+
+        Object[] elements = { id };
+
+        return this.query(query, elements);
+
+    }
+
+    private String getIDTable(String table) {
+
+        return switch(table) {
+
+            case "Cliente" -> "IDCliente = ?";
+
+            case "Factura" -> "IDFactura = ?";
+
+            case "Producto" -> "IDProducto = ?";
+
+            default -> this.getIDTable("Factura") + this.getIDTable("Producto");
+
+        };
+
     }
 
 }
---
+++ src/app/controllers/BackController.java
@@ -0,0 +1,82 @@
+package app.controllers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import app.DataBase;
+
+public class BackController {
+
+    public static BackController controller;
+    private static DataBase database;
+
+    private BackController(String user, String password) throws ClassNotFoundException, SQLException {
+        database = new DataBase(user, password);
+    }
+
+    public static void init(String[] args) throws ClassNotFoundException, SQLException {
+        controller = new BackController(args[0], args[1]);
+    }
+
+    public static void getProductos() throws SQLException {
+
+        ResultSet response = database.getAll("Producto");
+
+        while(response.next()) {
+            System.out.println(response.getString("Nombre"));
+        }
+
+        response.close();
+    }
+
+    public static int validarIngreso(String user, String password) {
+        return 1;
+    }
+
+}
+
+class DatabaseController {
+
+    private DataBase database;
+
+    public DatabaseController(String user, String password) throws ClassNotFoundException, SQLException {
+        this.database = new DataBase(user, password);
+    }
+
+    public ResultSet Producto() throws SQLException {
+
+        return this.database.getAll("Producto");
+
+    }
+
+    public ResultSet Producto(String id) throws SQLException {
+
+        return this.database.getByID("Producto", id);
+
+    }
+
+    public ResultSet Factura() throws SQLException {
+
+        return this.database.getAll("Factura");
+
+    }
+
+    public ResultSet Factura(String id) throws SQLException {
+
+        return this.database.getByID("Factura", id);
+
+    }
+
+    public ResultSet Cliente() throws SQLException {
+
+        return this.database.getAll("Cliente");
+
+    }
+
+    public ResultSet Cliente(String id) throws SQLException {
+
+        return this.database.getByID("Cliente", id);
+
+    }
+
+}
